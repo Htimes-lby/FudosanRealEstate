6af78ecd0f1327e4876960a8b95ea3135dbe2ba2
Ikiketa <ikiketamadoka@gmail.com>
2024-01-03 18:42:11 +0000

    fix: Ikekita/component/ItemSmallCard

--- Front-end/src/components/BasicTable.jsx
+++ Front-end/src/components/BasicTable.jsx
@@ -4,30 +4,30 @@ const BasicTable =  () =>{
     return(
         <table className="border border-[#8d8d8d]">
 
-        <tbody className="text-center text-xl ">
+        <tbody className="text-center text-[10px] ">
             <tr className="bg-[#ececec] ">
-                <td className="px-10 py-1 border-r border-[#8d8d8d]">価格</td>
-                <td className="px-10 py-1">4,430万円</td>
+                <td className="px-5 py-1 border-r border-[#8d8d8d]">価格</td>
+                <td className="px-5 py-1">4,430万円</td>
             </tr>
             <tr>
-                <td className="px-10 py-1 border-r border-[#8d8d8d]">間取り</td>
-                <td className="px-10 py-1">4LDK</td>
+                <td className="px-5 py-1 border-r border-[#8d8d8d]">間取り</td>
+                <td className="px-5 py-1">4LDK</td>
             </tr>
             <tr className="bg-[#ececec]"> 
-                <td className="px-10 py-1 border-r border-[#8d8d8d]">土地面積</td>
-                <td className="px-10 py-1">153.33m²</td>
+                <td className="px-5 py-1 border-r border-[#8d8d8d]">土地面積</td>
+                <td className="px-5 py-1">153.33m²</td>
             </tr>
             <tr>
-                <td className="px-10 py-1 border-r border-[#8d8d8d]">建物面積</td>
-                <td className="px-10 py-1">92.75m²</td>
+                <td className="px-5 py-1 border-r border-[#8d8d8d]">建物面積</td>
+                <td className="px-5 py-1">92.75m²</td>
             </tr>
             <tr className="bg-[#ececec]">
-                <td className="px-10 py-1 border-r border-[#8d8d8d]" >入居</td>
-                <td className="px-10 py-1">即日</td>
+                <td className="px-5 py-1 border-r border-[#8d8d8d]" >入居</td>
+                <td className="px-5 py-1">即日</td>
             </tr>
             <tr>
-                <td className="px-10 py-1 border-r border-[#8d8d8d]">駐車場</td>
-                <td className="px-10 py-1">3台</td>
+                <td className="px-5 py-1 border-r border-[#8d8d8d]">駐車場</td>
+                <td className="px-5 py-1">3台</td>
             </tr>
             
         </tbody>
--- Front-end/src/components/ItemSmallCard.jsx
+++ Front-end/src/components/ItemSmallCard.jsx
@@ -5,23 +5,19 @@ import photo from "../assets/img/photo.png"
 
 const ItemSmallCard = () =>{
     return(
-        <div className="w-96 ">
-            <div className="p-4">
-            <ShowContents 
-            text="テキスト テキスト テキスト テキスト
-                  テキスト テキスト テキスト テキスト
-                  テキスト テキスト テキスト テキスト
-                  テキスト テキスト テキスト テキスト
-                  テキスト テキスト テキスト テキスト
-                  テキスト テキスト テキスト テキスト"
-                  maxLength={100}
-                />
+        <div className="w-[240px]  border pb-3 shadow-lg ">
+            <div className="px-9 py-3 text-center text-[10px]" >
+                <ShowContents text=" 
+                テキスト テキスト テキスト テキスト
+                テキスト テキスト テキスト テキスト
+                テキスト テキスト テキスト テキスト
+                " maxLength="60"/>
             </div>
             <div className="flex justify-center p-4">
 
-            <img src={photo} alt="photo" className="w-80 " />
+            <img src={photo} alt="photo" className="w-40 " />
             </div>
-            <div className="pl-[20px]">
+            <div className="flex justify-center ">
             <BasicTable  />
             </div>
         </div>
