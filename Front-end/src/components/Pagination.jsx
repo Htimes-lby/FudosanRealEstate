de699702c3d2a8ded7c045c94af0f6c04c3ad2f1
ikekita <ikekitamadoka@gmail.com>
2024-01-04 23:26:15 +0900

    feat: Create category component

---
+++ Front-end/src/components/Pagination.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Category from "./Category";
+import img from '../assets/img/photo3.jpg'
+
+const Pagination = () =>{
+    return(
+        <Category img={img} text='一戸建て' />
+    )
+}
+
+export default Pagination;
