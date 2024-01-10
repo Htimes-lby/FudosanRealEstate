c220dbc58bcc317d0a6cfa3ff18c2b808f63b203
Sasuke Ichiro <ichirosasuke1@gmail.com>
2024-01-10 21:50:34 +0900

    feat: create-new-component-dashboard-eyecatch

--- Front-end/src/pages/DashboardPage/DashboardFeedbackBoard.jsx
+++ Front-end/src/pages/DashboardPage/DashboardFeedbackBoard.jsx
@@ -1,1 +1,10 @@
- 
+ import { React } from 'react';
+
+ const DashboardFeedbackBoard = () => {
+
+    return (
+        <div>This is DashboardFeedbackBoard</div>
+    );
+ }
+
+ export default DashboardFeedbackBoard;
