3ba9c86d0200f2d87a601c518941d00e8ff2f3c2
Sasuke Ichiro <ichirosasuke1@gmail.com>
2024-01-10 22:30:24 +0900

    refactor-realestate-small-card

--- Front-end/src/components/RealEstateSmallCard.jsx
+++ Front-end/src/components/RealEstateSmallCard.jsx
@@ -1,25 +1,23 @@
 import React from "react";
-import BasicTable from "./BasicTable";
-import photo from "../assets/img/photo.png"
+import BasicTableBuilding from "./BasicTableBuilding";
+import BasicTableLand from "./BasicTableLand";
 
-const ItemSmallCard = () =>{
+
+const ItemSmallCard = ( props ) =>{
+
+    const {briefDescription, image, tableData, realEstateCategory} = props;
     return(
         <div className="w-[240px]  border pb-3 shadow-lg ">
             <div className="px-9 py-3 text-center text-[10px] line-clamp-3" >
-               <p>
-
-                テキスト テキスト テキスト テキスト
-                テキスト テキスト テキスト テキスト
-                テキスト テキスト テキスト テキスト
-               </p>
-               
+                <p>{briefDescription}</p>
             </div>
             <div className="flex justify-center p-4">
-
-            <img src={photo} alt="photo" className="w-40 " />
+                <img src={image} alt="photo" className="w-40 " />
             </div>
             <div className="flex justify-center ">
-            <BasicTable  />
+                {
+                realEstateCategory == 'building' ? <BasicTableBuilding tableData = { tableData } /> : <BasicTableLand tableData = { tableData }/>
+                }
             </div>
         </div>
     )
