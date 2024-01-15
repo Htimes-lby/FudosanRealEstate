afd1b8b1484c86eef427501556e91bc1e7aa5668
Sasuke Ichiro <ichirosasuke1@gmail.com>
2024-01-15 15:04:15 +0900

    refactor: make component of the favourite Button

---
+++ Front-end/src/components/FavouriteButton.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import FavouriteStarInButton from "./FavouriteStarInButton"
+
+const addFavourite = 'お気に入りに追加'
+const removeFavourite = 'お気に入りを削除'
+
+const FavouriteButton = (props) => {
+
+    const {calledComponent} = props;
+    const [favouriteButtonClicked, setFavouriteButtonClicked] = useState(false);
+    const handleFavouriteButtonClicked = () => {
+        setFavouriteButtonClicked(favouriteButtonClicked ? false : true);
+    }
+
+    return(
+        <div className={`flex justify-center gap-[20px] items-center text-[#2A6484] bg-white text-[24px] rounded-xl border-[#2A6484] border-2 font-normal cursor-pointer ${calledComponent == 'realEstateDetailPage'? 'w-[380px] h-[80px]' : ''}`} onClick={handleFavouriteButtonClicked}>
+            <FavouriteStarInButton clicked = {favouriteButtonClicked}/>
+            <span>{favouriteButtonClicked ? removeFavourite : addFavourite}</span>
+        </div>
+    )
+}
+
+export default FavouriteButton;
