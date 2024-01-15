60252010a23332a2e7eb93a45cbccfd6ef782207
Ikekita <ikekitamadoka@gmail.com>
2024-01-16 01:48:29 +0900

    feat: ikekita/create/messageboardpage

--- Front-end/src/components/MessageItem.jsx
+++ Front-end/src/components/MessageItem.jsx
@@ -1,15 +1,25 @@
 import { useEffect,React } from "react";
 
+
 const MessageItem = (props) => {
     const {messageItem} = props;
-    const { username, content, date} = messageItem;
+    const { username, content, date ,status} = messageItem;
 
     return(
-        <div className="w-full h-[80px] flex justify-between items-center px-[20px]">
-            <div className=" text-[16px]">{username}</div>
-            <div className="line-clamp-3  w-[60%] max-h-[80px] text-[14px]">{content}</div>
-            <div className=" text-[16px]">{date}</div>
-        </div>
+            <div className="w-full flex justify-between items-center px-[20px]">
+                {status === "receive" ? 
+                <div className="flex gap-[80px] bg-[#F2ECCD]/60">
+                    <div className=" text-[16px]">{username}</div>
+                    <div className="line-clamp-3  w-[60%] max-h-[80px] text-[14px]">{content}</div>
+                    <div className=" flex items-end text-[16px]">{date}</div>
+                </div>:
+                <div className="flex gap-[80px]">
+                    <div className=" text-[16px]">{username}</div>
+                    <div className="line-clamp-3  w-[60%] max-h-[80px] text-[14px]">{content}</div>
+                    <div className=" flex items-end text-[16px]">{date}</div>
+                </div>
+                }
+            </div>
     )
 }
 
