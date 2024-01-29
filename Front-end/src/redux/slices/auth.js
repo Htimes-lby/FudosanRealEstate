0ace57eb9cd1588fbb0c397dcb5a82b377776cec
stormrageqb <stormrage.qb@gmail.com>
2024-01-29 11:39:39 +0900

    refactor: modified redux basic structure

--- Front-end/src/redux/slices/auth.js
+++ Front-end/src/redux/slices/auth.js
@@ -1,6 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
-import jwtDecode from "jwt-decode";
+import {jwtDecode} from "jwt-decode";
 import { setAuthToken } from "../utils";
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+
+export const signin = createAsyncThunk("/user/signin", async (payload) => {
+  try {
+    const response = await axios.get("/user", {
+      params: { userId: payload.userId, password: payload.password },
+    });
+    return response.data;
+  }
+  catch (e) {
+    return e.response.data;
+  }
+});
 
 const token = localStorage.token || null;
 const user = token ? jwtDecode(token) : null;
@@ -49,6 +64,21 @@ const authSlice = createSlice({
       state.isAuthenticated = false;
       state.user = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signin.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signin.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(signin.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
