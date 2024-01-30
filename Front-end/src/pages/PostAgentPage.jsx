4af87e27f54cbbf0bb01a59d2915e2153c542d84
Ikekita <ikekitamadoka@gmail.com>
2024-01-31 01:09:52 +0900

    fix: ikekita/backend/postCtr

--- Front-end/src/pages/PostAgentPage.jsx
+++ Front-end/src/pages/PostAgentPage.jsx
@@ -5,6 +5,16 @@ import ConditionForm from '../components/Form/ConditionForm';
 const PostAgentPage = () => {
 
     const [value, setValue] = useState('');
+    const [uploadDataArray, setUploadDataArray] = useState([]);
+    const [conditionData, setConditionData] = useState("");
+    const handleUploadDataArray = (data) => {
+        setUploadDataArray(data);
+    };
+
+    const handleconditionDataArray = (data) => {
+        setConditionData(data);
+    };
+    
     return (
         <div className='bg-[#F1F1F1]  pt-[124px]'>
             <div className='container bg-white'>
@@ -21,54 +31,54 @@ const PostAgentPage = () => {
                     
                     </div>
                 </div>
-                <div className=' w-[900px] ml-[310px]  flex gap-[169px]'>
-                    <div className='w-[177px] flex gap-[35px]  '>
-                        <span className='bg-[#F69191] h-[28px] inline-block p-1 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
+                <div className=' w-[900px] ml-[310px]  flex gap-[157px]'>
+                    <div className='w-[190px] flex gap-[35px]  '>
+                        <span className='bg-[#F69191] h-[28px] flex items-center p-2 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
                         <p className='text-[20px]'>担当者名</p>
                     </div>
                     <div className='flex'>                      
-                        <input type="text"  className='w-[445px] border-2 border-black rounded-md'/>
+                        <input type="text"  className='w-[445px] border-[1px] focus:outline-none focus:border-blue-500 p-1 border-black rounded-md'/>
                     </div>
                 </div>
                 <div className=' w-[900px] ml-[310px]  flex gap-[169px] mt-[40px]'>
                     <div className='w-[177px] flex gap-[35px]  '>
-                        <span className='bg-[#F69191] h-[28px] inline-block p-1 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
+                        <span className='bg-[#F69191] h-[28px] flex items-center p-2 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
                         <p className='text-[20px]'>会社名</p>
                     </div>
                     <div className='flex'>                      
-                        <input type="text"  className='w-[445px] border-2 border-black rounded-md'/>
+                        <input type="text"  className='w-[445px] border-[1px] focus:outline-none focus:border-blue-500 p-1 border-black rounded-md'/>
                     </div>
                 </div>
                 <div className=' w-[900px] ml-[310px]  flex gap-[169px] mt-[40px]'>
                     <div className='w-[177px] flex gap-[35px]  '>
-                        <span className='bg-[#F69191] h-[28px] inline-block p-1 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
+                        <span className='bg-[#F69191] h-[28px] flex items-center p-2 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
                         <p className='text-[20px]'>メール</p>
                     </div>
                     <div className='flex'>                      
-                        <input type="text"  className='w-[445px] border-2 border-black rounded-md'/>
+                        <input type="text"  className='w-[445px] border-[1px] focus:outline-none focus:border-blue-500 p-1 border-black rounded-md'/>
                     </div>
                 </div>
                 <div className=' w-[900px] ml-[310px]  flex gap-[169px] mt-[40px]'>
                     <div className='w-[177px] flex gap-[35px]  '>
-                        <span className='bg-[#F69191] h-[28px] inline-block p-1 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
+                        <span className='bg-[#F69191] h-[28px] flex items-center p-2 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
                         <p className='text-[20px]'>住所</p>
                     </div>
                     <div className='flex'>                      
-                        <input type="text"  className='w-[445px] border-2 border-black rounded-md'/>
+                        <input type="text"  className='w-[445px] border-[1px] focus:outline-none focus:border-blue-500 p-1 border-black rounded-md'/>
                     </div>
                 </div>
 
                 <div className=' w-[900px] ml-[310px]  flex gap-[169px] pt-[40px] '> 
 
                     <div className='w-[177px] flex gap-[35px]  '>
-                            <span className='bg-[#F69191] h-[28px] inline-block p-1 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
+                            <span className='bg-[#F69191] h-[28px] flex items-center p-2 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
                             <p className="text-[20px] ">役割</p>
                             
                     </div> 
                     <div className=" flex items-center  ">
                             
                         <select
-                            className="border-2 rounded-md border-black w-[445px] "
+                            className="border-[1px] focus:outline-none focus:border-blue-500 p-1 rounded-md border-black w-[445px] "
                             onChange={event => setValue(event.target.value)}
                             defaultValue={value}>                       
                             <option className="text-[16px]"  value="" >&nbsp;</option>
@@ -82,34 +92,34 @@ const PostAgentPage = () => {
                 </div>
 
 
-                <div className=' w-[900px] ml-[310px]  flex gap-[169px] pt-[40px]'>
-                    <div className='w-[177px] flex gap-[35px]  '>
-                        <span className='bg-[#F69191] h-[28px] inline-block p-1 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
+                <div className=' w-[900px] ml-[310px]  flex gap-[157px] pt-[40px]'>
+                    <div className='w-[190px] flex gap-[35px]  '>
+                        <span className='bg-[#F69191] h-[28px] flex items-center p-2 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
                         <p className='text-[20px]'>電話番号</p>
                     </div>
                     <div className='flex '>
-                        <input type="text" className='w-[99px] border-2 border-black rounded-md'/>
+                        <input type="text" className='w-[99px] border-[1px] focus:outline-none focus:border-blue-500 p-1 border-black rounded-md'/>
                         <span className='border-t-2 w-[30px] border-black mt-[15px] mx-[22px]'></span>            
-                        <input type="text" className='w-[99px] border-2 border-black rounded-md'/>
+                        <input type="text" className='w-[99px] border-[1px] focus:outline-none focus:border-blue-500 p-1 border-black rounded-md'/>
                         <span className='border-t-2 w-[30px] border-black mt-[15px] mx-[21px]'></span>
-                        <input type="text" className='w-[99px] border-2 border-black rounded-md'/>
+                        <input type="text" className='w-[99px] border-[1px] focus:outline-none focus:border-blue-500 p-1 border-black rounded-md'/>
                     </div>
                 </div>
-                <div className=' w-[900px] ml-[310px]  flex gap-[169px] mt-[40px]'>
-                    <div className='w-[177px] flex gap-[35px]  '>
-                        <span className='bg-[#F69191] h-[28px] inline-block p-1 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
+                <div className=' w-[900px] ml-[310px]  flex gap-[157px] mt-[40px]'>
+                    <div className='w-[190px] flex gap-[35px]  '>
+                        <span className='bg-[#F69191] h-[28px] flex items-center p-2 rounded-md text-[15px] mx-[12px] text-white'>必須</span>
                         <p className='text-[20px]'>投稿内容</p>
                     </div>
                     <div className='flex'>                      
-                        <textarea type="text"  className='w-[445px] h-[140px] border-2 border-black rounded-md'/>
+                        <textarea type="text"  className='w-[445px] h-[140px] border-[1px] focus:outline-none focus:border-blue-500 p-1 border-black rounded-md'/>
                     </div>
                 </div>
 
                 <div className=' w-[900px] ml-[310px]  flex gap-[169px] mt-[40px]'>
-                    <UploadImageForm title="資格のコピー" button="ファイル選択" gap={"gap-[35px]"} status={"必須"} />
+                <UploadImageForm title="物件写真" button="画像ファイルを選択する" width1={"w-[800px]"} width2={"w-[300px]"} gap={"gap-[35px]"} onDataArrayFromChild={handleUploadDataArray} />
                 </div>
                 <div className='flex justify-center'>
-                    <ConditionForm />
+                    <ConditionForm onDataArrayFromChild={handleconditionDataArray} />
                 </div>
                 <div className='flex justify-center pt-[105px] pb-[170px]'>
                     <button className='bg-[#2A6484] text-white px-[115px] py-[14px] text-[24px] rounded-[20px]'>提出</button>
