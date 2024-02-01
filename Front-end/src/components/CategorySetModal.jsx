41b820afded545dc75a7324fd56aab540230eee6
stormrageqb <stormrage.qb@gmail.com>
2024-02-01 17:11:51 +0900

    feat: implement admin realestate approve toggle

--- Front-end/src/components/CategorySetModal.jsx
+++ Front-end/src/components/CategorySetModal.jsx
@@ -6,12 +6,9 @@ const CategorySetModal = (props) => {
 
     const modalRef = useRef(null);
     const history = useHistory();
-    const [category, setCategory] = useState('');
-
-    const handleToggleShowModal = props.handleToggleShowModal;
-    const approved = props.approved;
-    const handleApproveToggle = props.handleApproveToggle;
-    const approveToggleButtonContent = approved ? '掲示板に投稿する' : '掲示板に投稿する';
+    const setCategory = props.setCategory;
+    const makeApproveRequest = props.makeApproveRequest;
+    const setShowCategorySetModal = props.setShowCategorySetModal;
 
     // const handleContactPosterButtonClicked = (props) => {
     //     const agentId = props;
@@ -21,7 +18,9 @@ const CategorySetModal = (props) => {
     // }
 
     const handleDocumentClick = (event) => {
-        if(modalRef.current && !modalRef.current.contains(event.target)){handleToggleShowModal(false)}
+        if(modalRef.current && !modalRef.current.contains(event.target)){
+            setShowCategorySetModal(false);
+        }
     }
 
     useEffect(() => {
@@ -33,15 +32,14 @@ const CategorySetModal = (props) => {
 
     return(
         <>
-        <div ref={modalRef} className='absolute bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/3 flex flex-col items-center w-[450px] pt-16 bg-white rounded-xl noto-regular z-50'>
-            <span className='text-2xl'>カテゴリーを選択してください。</span>
-            <div className='flex justify-center pt-8 gap-7'>
+        <div ref={modalRef} className='fixed bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/3 flex flex-col items-center w-[450px] pt-16 bg-white rounded-xl noto-regular z-50'>
+            <span className='text-2xl noto-medium'>カテゴリーを選択してください。</span>
+            <div className='flex justify-center pt-12 gap-7'>
                 <div className=" flex items-center justify-center">
-                        
                         <select
                             className="border-[1px] focus:outline-none focus:border-blue-500 p-1 rounded-md border-black w-[272px]"
                             onChange={event => setCategory(event.target.value)}
-                            defaultValue={category}>                       
+                            defaultValue={''}>                       
                             <option className="text-[16px]"  value="" >&nbsp;</option>
                             <option className="text-[16px]"  value="古民家" >&nbsp;古民家</option>
                             <option className="text-[16px]"  value="マンション・ビル" >&nbsp;マンション・ビル</option>
@@ -51,15 +49,10 @@ const CategorySetModal = (props) => {
                             <option className="text-[16px]"  value="林野" >&nbsp;林野</option>
                             <option className="text-[16px]"  value="農地" >&nbsp;農地</option>
                             <option className="text-[16px]"  value="住宅地" >&nbsp;住宅地</option>
-                            
-
                         </select>
                 </div>
             </div>
-            
-            
-            <div className='flex w-[250px]  h-[40px] justify-center items-center bg-[#2A6484] text-white text-lg noto-medium rounded-xl cursor-pointer mt-4 mb-[40px]' onClick={handleApproveToggle} >掲示板に投稿する</div>
-        
+            <div className='flex w-[250px]  h-[50px] justify-center items-center bg-[#2A6484] text-white text-lg noto-medium rounded-xl cursor-pointer mt-8 mb-[40px]' onClick={makeApproveRequest} >掲示板に投稿する</div>
         </div>
         <div className='opacity-75 bg-black fixed inset-0 z-40'></div>
         </>
