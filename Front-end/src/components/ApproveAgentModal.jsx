b489fecfc8f5c3c29dd6f78358ba54d2d13eaba5
stormrageqb <stormrage.qb@gmail.com>
2024-02-02 07:24:12 +0900

    feat: admin approve agent

--- Front-end/src/components/ApproveAgentModal.jsx
+++ Front-end/src/components/ApproveAgentModal.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
 
@@ -8,8 +9,9 @@ const ApproveAgentModal = (props) => {
     const history = useHistory();
 
     const agent = props.agent;
+    const [approved, setApproved] = useState(agent.approved)
     const handleToggleShowModal = props.handleToggleShowModal;
-    const approved = props.approved;
+    console.log('-------------------------------', approved)
     const handleApproveToggle = props.handleApproveToggle;
     const approveToggleButtonContent = approved ? '掲示板に投稿する' : '掲示板に投稿する';
 
@@ -24,6 +26,28 @@ const ApproveAgentModal = (props) => {
         if(modalRef.current && !modalRef.current.contains(event.target)){handleToggleShowModal(false)}
     }
 
+    const handleApproveToggleBtnClicked = async () => {
+        if(approved) {
+            try {
+                const agentId = agent._id;
+                const res = await axios.post('/disapproveAgent', {agentId});
+                setApproved((prevState) => !prevState);
+                
+            } catch (error) {
+                console.log({error: error.message});
+            }
+        } else {
+            try {
+                const agentId = agent._id;
+                const res = await axios.post('/approveAgent', {agentId});
+                setApproved((prevState) => !prevState);
+                
+            } catch (error) {
+                console.log({ error: error.message });
+            }
+        }
+    }
+
     useEffect(() => {
         document.addEventListener('mousedown', handleDocumentClick);
         return () => {
@@ -33,22 +57,28 @@ const ApproveAgentModal = (props) => {
 
     return(
         <>
-        <div ref={modalRef} className='absolute bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/3 flex flex-col items-center w-[450px] pt-16 bg-white rounded-xl noto-regular z-50'>
-            <span className='text-2xl'>{`${agent.agentName}   ${agent.companyName}`}</span>
-            <div className='flex justify-center pt-8 gap-7'>
-                <span>{agent.phoneNumber}</span>
-                <span>{agent.email}</span>
+        <div ref={modalRef} className='fixed bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/2 flex flex-col items-center w-[450px] pt-16 bg-white rounded-xl noto-regular z-50'>
+            <div className='flex justify-center gap-6'>
+                <span className='text-xl'>{agent.agentName.lastNameGanji} {agent.agentName.firstNameGanji}</span>
+                <span className=' text-xl'>{agent.companyName}</span>
             </div>
-            <div className='flex justify-center gap-5'>
+
+            <div className='flex justify-center gap-5 mt-3'>
                 <span>{agent.address.province}</span>
                 <span>{agent.address.city}</span>
                 <span>{agent.address.street}</span>
             </div>
-            <span className='w-[420px] mx-auto p-3 text-sm'>{agent.content}</span>
+
+            <div className='flex justify-center mt-1 gap-7'>
+                <span>{agent.phoneNumber}</span>
+                <span>{agent.agentEmail}</span>
+            </div>
+            
+            <span className='w-[420px] mx-auto p-3 text-sm mt-4'>{agent.content}</span>
             <div className='flex w-[250px] h-[40px] justify-center items-center bg-[#2A6484] text-white text-lg noto-medium rounded-xl cursor-pointer mt-4 mb-2' onClick={() => handleContactPosterButtonClicked(agent._id)}>投稿者への連絡</div>
             {/* <div className='flex w-[250px] h-[40px] justify-center items-center bg-[#2A6484] text-white text-[24px] rounded-xl cursor-pointer' onClick={() => handleContactPosterButtonClicked(agent._id)}>Contact to Poster</div> */}
             {/* <div className='flex w-[250px] h-[40px] justify-center items-center bg-[#2A6484] text-white text-[24px] rounded-xl cursor-pointer' onClick={() => handleApproveToggle}>{approveToggleButtonContent}</div> */}
-            <div className='flex w-[250px] h-[40px] justify-center items-center text-[#2A6484] bg-white text-lg noto-medium rounded-xl cursor-pointer mb-5 border-[2px] border-[#2A6484]' onClick={handleApproveToggle}>{approveToggleButtonContent}</div>
+            <div className='flex w-[250px] h-[40px] justify-center items-center text-[#2A6484] bg-white text-lg noto-medium rounded-xl cursor-pointer mb-5 border-[2px] border-[#2A6484]' onClick={handleApproveToggleBtnClicked}>{approved ? '掲示板に削除する' : '掲示板に投稿する'}</div>
             <div></div>
         </div>
         <div className='opacity-75 bg-black fixed inset-0 z-40'></div>
--- Front-end/src/pages/AdminViewAgentPage.jsx
+++ Front-end/src/pages/AdminViewAgentPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ApproveAgentModal from '../components/ApproveAgentModal';
+import axios from 'axios';
 const agents = [
     {
     _id:'123',
@@ -141,9 +142,10 @@ const agents = [
 ]
 const AdminViewAgentPage = () => {
 
-    const [activeAgentCategory, setActiveAgentCategory] = useState('judicialscrivener');
+    const [activeAgentCategory, setActiveAgentCategory] = useState('司法書士');
     const [showApproveAgentModal, setShowApproveAgentModal] = useState(false);
     const [selectedAgent, setSelectedAgent] = useState();
+    const [agents, setAgents] = useState(null);
     const [approved, setApproved] = useState(false);
     const handleAgentClicked = (index) => {
         setSelectedAgent(agents[index]);
@@ -156,24 +158,42 @@ const AdminViewAgentPage = () => {
         console.log('I am here', approved);
         setApproved((prevState) => !prevState);
     }
+    const fetchAgentData = async () => {
+        const res = await axios.get('/getAgentByAdmin');
+        console.log('--------------------------------', res.data)
+        setAgents(res.data);
+    }
+
+    useEffect (() => {
+        if(showApproveAgentModal === false) {
+            fetchAgentData();
+        }
+    },[showApproveAgentModal])
+
+    if(agents === null) {
+        return (
+            <div>loading................</div>
+        )
+    }
     return (
         <div className='relative flex flex-col items-center w-full pt-20 bg-[#F1F1F1]'>
             <div className='text-center text-[32px] font-semibold'>エージェントを見る</div>
             <div className='flex relative border-b-2 pt-10 border-black w-[500px] transition-all duration-300'>
-                <span className=' w-[33%] cursor-pointer text-center pb-3' onClick={() => setActiveAgentCategory('realEstateAgent')}>不動産業者</span>
-                <span className=' w-[34%] cursor-pointer text-center' onClick={() => setActiveAgentCategory('judicialscrivener')}>司法書士</span>
-                <span className=' w-[33%] cursor-pointer text-center' onClick={() => setActiveAgentCategory('invester')}>投資家</span>
-                <div className={`absolute w-[33.3%] h-1 bottom-0 bg-[#f13f13] rounded-md transition-all duration-500 ${activeAgentCategory === 'realEstateAgent' ? 'left-0' : activeAgentCategory === 'judicialscrivener' ? 'left-[33.3%]' : 'left-[66.6%]' }`}></div>
+                <span className=' w-[33%] cursor-pointer text-center pb-3' onClick={() => setActiveAgentCategory('不動産業者')}>不動産業者</span>
+                <span className=' w-[34%] cursor-pointer text-center' onClick={() => setActiveAgentCategory('司法書士')}>司法書士</span>
+                <span className=' w-[33%] cursor-pointer text-center' onClick={() => setActiveAgentCategory('投資家')}>投資家</span>
+                <div className={`absolute w-[33.3%] h-1 bottom-0 bg-[#f13f13] rounded-md transition-all duration-500 ${activeAgentCategory === '不動産業者' ? 'left-0' : activeAgentCategory === '司法書士' ? 'left-[33.3%]' : 'left-[66.6%]' }`}></div>
             </div>
-            <div className='grid grid-cols-2 gap-x-16 gap-y-5 max-w-[1250px] pt-10 pb-24'>
+            <div className='grid grid-cols-2 gap-x-16 gap-y-5 max-w-[1250px] min-h-[600px] pt-10 pb-24'>
             {
                 agents.map((agent, index) => (
+                    agent.category === activeAgentCategory &&
                     <div key={index} className='flex flex-col items-center w-[500px] px-4 py-3 bg-white rounded-lg border-[1px] border-black/30 cursor-pointer' onClick={() => handleAgentClicked(index)}>
                         <div className='flex items-center justify-between w-full'>
-                            <div>{agent.agentName} {agent.companyName}</div>
+                            <div> {agent.agentName.firstNameGanji} {agent.companyName}</div>
                             <div className='flex flex-col items-end gap-[1px]'>
                                 <span>{agent.phoneNumber}</span>
-                                <span>{agent.email}</span>
+                                <span className='text-sm'>{agent.agentEmail}</span>
                             </div>
                         </div>
                         <div className='text-xs pt-3 line-clamp-3'>{agent.content}</div>
