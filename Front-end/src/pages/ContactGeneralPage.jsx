412172686bab4c9aaad77e85eafcb0306c7223df
sasukeichiro <sasukeichiro1@gmail.com>
2024-02-05 08:59:21 +0900

    feat: integrate contact function with api

--- Front-end/src/pages/ContactGeneralPage.jsx
+++ Front-end/src/pages/ContactGeneralPage.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-const contactMessages = [
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import { useHistory, useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+
+const tempContactMessages = [
     {
         sender: 'minato',
         receiver: 'fudosan',
@@ -24,10 +28,70 @@ const contactMessages = [
         ふどさん事務局 齊藤`
     },
 ]
-const ContactGeneralPage = ({senderName, receiveName}) => {
+
+const ContactGeneralPage = () => {
     const history = useHistory();
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+
+    const [cookies, setCookie] = useCookies();
+    const [clientId, setClientId] = useState();
+    const myId = cookies.user._id;
+    const myName = cookies.user.name.lastNameGanji + ' ' + cookies.user.name.firstNameGanji;
+    // const isAdmin = cookies.user.isAdmin;
+
+    const [content, setContent] = useState();
+    const [contactMessages, setContactMessages] = useState([]);
+
     const handleNavigateToFaqClicked = () => {
         history.push('/faq');
+    }
+
+    const sendContactMessage = async () => {
+        try {
+            // const category = isAdmin ? 'reply' : 'query';
+            const category = 'query';
+            const payload = {
+                content: content,
+                clientId: myId,
+                category: category,
+            }
+            const res = await axios.post('/saveGeneralContactMessage', payload);
+            console.log('=====================================', res)
+            // setContactMessages(prevMessages => [...prevMessages, content]);
+            } catch (error) {
+                console.log(error.message);
+            }
+    }
+
+    const fetchContactMessages = async () => {
+        try {
+            const clientId = myId;
+            console.log('++++++++++++++++==================', clientId)
+            const params = new URLSearchParams({'clientId': clientId}).toString();
+            const res = await axios.get(`/fetchGeneralContactMessages?${params}`);
+            console.log('+++++++++++++++++++++++++++++++++++', res);
+            setContactMessages(res.data.contactMessages);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        // if(isAdmin) {
+        //     const tempClientId = searchParams.get('clientId');
+        //     setClientId(tempClientId);
+        // } else {
+        //     setClientId(myId);
+        // }
+        setClientId(myId);
+        fetchContactMessages();
+    },[])
+
+    if(contactMessages === null) {
+        return(
+            <div>loading....................</div>
+        )
     }
 
     return (
@@ -44,16 +108,20 @@ const ContactGeneralPage = ({senderName, receiveName}) => {
                 <p className='inline-block py-10 px-[15px] underline underline-offset-8 text-[24px] font-normal cursor-pointer' onClick={handleNavigateToFaqClicked}>良くあるご質問はこちら <p className = 'pr-3 fa-solid fa-arrow-right underline underline-offset-8 '></p></p>
                 {
                     contactMessages.map((contactMessage, index) =>
-                        (<div key={index} className={`w-full h-auto border-2 border-[#2A6484]/40 p-10 mb-10 ${contactMessage.sender === 'minato' ? 'bg-white' : 'bg-[#F2ECCD]'}`}>
-                            <p className='text-[20px] font-semibold mb-8'>{contactMessage.sender}さん → {contactMessage.receiver}さん</p>
+                        (<div key={index} className={`w-full h-auto border-2 border-[#2A6484]/40 p-10 mb-10 ${contactMessage.category === 'query' ? 'bg-white' : 'bg-[#F2ECCD]'}`}>
+                            {
+                                contactMessage.category === 'query' ?
+                                <p className='text-[20px] font-semibold mb-8'>{myName}さん → ふどさんさん</p> :
+                                <p className='text-[20px] font-semibold mb-8'>ふどさんさん → {myName}さん</p>
+                            }
                             <p className='text-[16px]'>{contactMessage.content}</p>
                         </div>)
                     )
                 }
                 <p className='mt-10 mb-3 text-[24px] pb-[15px]'>メッセージ送信フォーム</p>
-                <textarea name="message" id="message" cols="10" rows="3" className='w-full py-4 px-12 text-[24px]'></textarea>
+                <textarea name="message" id="message" cols="10" rows="3" className='w-full py-4 px-12 text-[24px]' onChange={(e) => setContent(e.target.value)}></textarea>
                 <div className='pt-10 pb-24 flex justify-center'>
-                    <button className='py-3 px-8 rounded-[15px] bg-[#2A6484] noto-medium text-[24px] text-white'>メッセージを送信する</button>
+                    <button className='py-3 px-8 rounded-[15px] bg-[#2A6484] noto-medium text-[24px] text-white' onClick={sendContactMessage}>メッセージを送信する</button>
                 </div>
             </div>
         </div>
